6780686cb165c512edaa23f407e371ec38650548
no-u1 <63494383+no-u1@users.noreply.github.com>
2020-04-12 18:49:44 +1000

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,18 @@
+module.exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const msg = await message.channel.send("Ping?");
+  msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+};
+
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+module.exports.help = {
+  name: "ping",
+  category: "Miscelaneous",
+  description: "It like... Pings. Then Pongs. And it's not Ping Pong.",
+  usage:"ping"
+};
