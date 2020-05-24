bf914eb719c9e5c5aa8e15f837a73d1eda0a376a
no-u1 <63494383+no-u1@users.noreply.github.com>
2020-05-24 12:25:56 +0000

    Update server.js -- Update the code from glitch.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const express = require('express');
 const app = express();
-const leveling = require("./commands/events/leveling.json");
 app.get("/", (request, response) => {
   console.log(Date.now() + "Ping Received");
   response.sendStatus(200);
@@ -11,13 +10,14 @@ setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`); 
 }, 280000);
 
+const db = require('quick.db')
 const fs = require('fs')
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require("./config.json");
 const client = new Discord.Client();
 bot.commands = new Discord.Collection();
-
+bot.aliases = new Discord.Collection();
 fs.readdir('./commands/', (err, files) => {
 
   if(err) console.log(err);
@@ -37,6 +37,9 @@ fs.readdir('./commands/', (err, files) => {
     let props = require(`./commands/${f}`)
     console.log(`${f} loaded!`);
     bot.commands.set(props.help.name, props);
+    props.help.aliases.forEach(alias => {   
+    bot.aliases.set(alias, props.help.name);
+    })
   });
 
 
@@ -47,7 +50,7 @@ fs.readdir('./commands/', (err, files) => {
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("-help", {type: "Playing"});
+  bot.user.setActivity(`${bot.guilds.size} servers | -help`, {type: "Watching"});
 
 });
 
@@ -61,21 +64,41 @@ bot.on('guildMemberAdd', member => {
 });
 
 bot.on("message", async message => {
+  
+  if(message.author.bot)return;
+  if(message.channel.type === "dm")return;
+  let prefixes = JSON.parse(fs.readFileSync("./commands/events/prefixes.json"));
+  
+  
+  if(!prefixes[message.guild.id]){
+		prefixes[message.guild.id] = {
+			prefixes: "-"
+		}
+	}
+  
   if(message.author.bot) return;
-  let ids = ('385067115226595339')
+  let ids = ("385067115226595339")
   if (message.author.id === ids) return  
-  let prefix = config.prefix;
-  let messageArray = message.content.split(" ");
-  let command = messageArray[0];
-  let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(command.slice(prefix.length));
+  let prefix = prefixes[message.guild.id].prefixes || "-"
+  let args = message.content.slice(prefix.length).trim().split(' ');
+  let cmd = args.shift().toLowerCase();
+  let command; 
   if (!message.content.startsWith(prefix)) return;
-  if(commandfile) commandfile.run(bot,message,args);
+  if(bot.commands.has(cmd)) {
+    command = bot.commands.get(cmd);
+  } else {
+    command = bot.commands.get(bot.aliases.get(cmd));
+  }
+  
+  if (command) command.run(bot,message,args);
+  
+  //let commandfile = bot.commands.get(command.slice(prefix.length));
+  //if(commandfile) commandfile.run(bot,message,args);
   var author = message.author;
 })
 
 bot.on("message", (message) => {
-    let ids = ["198439918647771136", "644811269123211272", "547907138840690695","","588197117869162506","656310275171024896","172664698091601920","681001419880398866","418211560213970945","569308933030805504","691793056306823199"]
+    let ids = ["198439918647771136", "644811269123211272", "547907138840690695","","588197117869162506","656310275171024896","172664698091601920","681001419880398866","418211560213970945","569308933030805504","691793056306823199","555892702844157972"]
   if (ids.includes(message.author.id)) {
   if (!message.content.includes("-DadDelete-")) return; {
     message.delete();
@@ -84,7 +107,7 @@ bot.on("message", (message) => {
 });
 
 bot.on("message", (message) => {
-    let ids = ["198439918647771136", "644811269123211272", "547907138840690695","","588197117869162506","656310275171024896","172664698091601920","681001419880398866","418211560213970945","569308933030805504","691793056306823199"]
+    let ids = ["198439918647771136", "644811269123211272", "547907138840690695","","588197117869162506","656310275171024896","172664698091601920","681001419880398866","418211560213970945","569308933030805504","691793056306823199","555892702844157972"]
   if (ids.includes(message.author.id)) {
   if (!message.content.includes("-daddelete-")) return; {
     message.delete();
@@ -93,7 +116,7 @@ bot.on("message", (message) => {
 });
  
 bot.on("message", (message) => {
-    let ids = ["198439918647771136", "644811269123211272", "547907138840690695","","588197117869162506","656310275171024896","172664698091601920","681001419880398866","418211560213970945","569308933030805504","691793056306823199","446416793557270539"]
+    let ids = ["198439918647771136", "644811269123211272", "547907138840690695","","588197117869162506","656310275171024896","172664698091601920","681001419880398866","418211560213970945","569308933030805504","691793056306823199","446416793557270539","555892702844157972"]
   if (ids.includes(message.author.id)) {
   if (!message.content.includes("-dd-")) return; {     //Oh GG I should do that too, yes you should
     message.delete();
@@ -106,27 +129,51 @@ bot.on("message", async message => {
 let ids = ('385067115226595339')
 if (message.author.id === ids) return 
 if(message.author.bot) return;
-if (!leveling[message.author.id]){
-  leveling[message.author.id] = {
-    messagesSent: 0,
-    level: 0,
-    MessagesUntilNextLevel: 10
-  }
-}
+if(message.channel.type === "dm") return;
+  
 
-  leveling[message.author.id].messagesSent++;
-  
-  if (leveling[message.author.id].messagesSent >+ leveling[message.author.id].MessagesUntilNextLevel){
-    leveling[message.author.id].level++;
-    leveling[message.author.id].MessagesUntilNextLevel *= leveling.multi;
-    message.channel.send("Congrats " + message.author.toString() + " on reaching level " + leveling[message.author.id].level + "!");  
+
+
+
+  db.add(`messages_${message.guild.id}_${message.author.id}`, 1)
+
+  let messagefetch = db.fetch(`messages_${message.guild.id}_${message.author.id}`)
+
+
+
+  let messages;
+
+ if (messagefetch == 25) messages = 25; //Level 1
+
+  else if (messagefetch == 65) messages = 65; // Level 2
+  else if (messagefetch == 115) messages = 115; // Level 3
+  else if (messagefetch == 200) messages = 200; // Level 4
+  else if (messagefetch == 300) messages = 300; // Level 5
+  else if (messagefetch == 450) messages = 450; // Level 6
+  else if (messagefetch == 600) messages = 600; // Level 7
+  else if (messagefetch == 800) messages = 800; // Level 8
+  else if (messagefetch == 1025) messages = 1025; // Level 9
+  else if (messagefetch == 1265) messages = 1265; // Level 10
+
+
+
+  if (!isNaN(messages)) {
+
+    db.add(`level_${message.guild.id}_${message.author.id}`, 1)
+
+    let levelfetch = db.fetch(`level_${message.guild.id}_${message.author.id}`)
+
+    
+
+ 
+   message.channel.send(`Congratulations  ${message.author} On Reaching Level ${levelfetch} `)
+
   }
-  
-  fs.writeFileSync("./commands/events/leveling.json", JSON.stringify(leveling, null, 2), function(err){
-    if (err){
-      console.log(err);
-    }
-  });
+
+
+
+}) 
+ 
   
   bot.deletedMessages = new Discord.Collection()
 bot.on("messageDelete", msg => bot.deletedMessages.set(msg.channel.id, msg))
@@ -137,7 +184,7 @@ bot.on("messageDelete", msg => bot.deletedMessages.set(msg.channel.id, msg))
   console.log(`Totally ${bot.deletedMessages.size} deleted messages stored!`)
 })
   
-});
+
 
 bot.on("guildCreate", guild => {
     let channelID;
@@ -153,6 +200,12 @@ bot.on("guildCreate", guild => {
 
     let channel = bot.channels.get(guild.systemChannelID || channelID);
     channel.send("Thanks for inviting me into this server! The default prexfix is `-`");
+    bot.channels.get("709483048000815126").send(`I have joined a new server named **${guild.name}** (${guild.id})`);
 });
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") bot.channels.get("707071555641016371").send(`**${message.author.username}#${message.author.discriminator} (ID: ${message.author.id})**: ${message.content}`) 
+})
 
 bot.login(config.token);
